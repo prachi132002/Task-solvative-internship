2bb1f9f646e789330d446faad97d00a8273a62c0
prachi132002 <103041783+prachi132002@users.noreply.github.com>
2024-09-21 12:49:19 +0530

    change

--- solvative-stopwatch-app/src/App.jsx
+++ solvative-stopwatch-app/src/App.jsx
@@ -1,22 +1,11 @@
 import React from 'react'
+import Stopwatch from './Stopwatch'
 
 function App() {
-    const startbtn=()=>{
-
-    }
-    const stopbtn=()=>{
-
-    }
   return (
-    <div className='StopWatch'>
-    <h1>StopWatch App</h1>
-    <h2>StopWatch:0:00</h2>
-    <div>
-        <button  className ='start'onClick={startbtn}>Start</button>
-        <button className='stop' onClick={stopbtn}>stop</button>
-    </div>
-    </div>
-    
+    <>
+      <Stopwatch/>
+    </>
   )
 }
 
